bea97e9fda906051251ff740781a2f54eeac0463
Daniel Ramirez <dxas90@gmail.com>
2022-06-22 11:15:27 +0300

    updated treesitter

--- lua/dxas90/user/treesitter.lua
+++ lua/dxas90/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "c", "lua", "go", "python", "bash", "cpp", "css", "hcl", "html", "javascript", "json", "proto", "ruby", "toml" },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "java" }, -- List of parsers to ignore installing
   autopairs = {
